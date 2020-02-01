ad6c9c9029c77abc054022679307466865a5b4e6
CRAFTSTARCN <179680737@qq.com>
2020-02-01 22:33:47 +0800

    Add Got data of each province

--- nCov/grabber.py
+++ nCov/grabber.py
@@ -7,15 +7,42 @@ if __name__ == "__main__":
 import requests
 from bs4 import BeautifulSoup
 import urllib
+import json
 
+#获取全国信息
 infoLink = "https://3g.dxy.cn/newh5/view/pneumonia?"
 infoPage = requests.get(infoLink).content.decode('utf-8')
 infoSoup = BeautifulSoup(infoPage, 'lxml')
 infoList = infoSoup.select('#getStatisticsService')
 infoStr  = infoList[0].text
-def findData(a:str)->str():
+def findData(a:str)->str:
     tagPosition = infoStr.find(a)
     mapPosition = infoStr.find(':',tagPosition)
     endPistionn = infoStr.find(',',mapPosition)
     return infoStr[mapPosition+1 : endPistionn]
 
+
+#获取Json
+provJsonTag   = infoSoup.select("#getAreaStat")[0].text
+provJsonStart = provJsonTag.find("[")
+provJsonEnd   = provJsonTag.rfind("]")
+provJsonStr   = provJsonTag[provJsonStart:provJsonEnd+1]
+
+
+#写入Json
+with open("data.json",'w', encoding='utf-8') as jsFile:
+    jsFile.write(provJsonStr)
+
+#读取Json
+with open("data.json",'r', encoding='utf-8') as jsFile:
+    jsObj = json.load(jsFile)
+
+provNames   = list()
+provNumber  = list()
+
+for maps in jsObj :
+    provNames.append(maps['provinceShortName'])
+    provNumber.append(maps['confirmedCount'])
+
+for na,nu in zip(provNames,provNumber) :
+    print(na,":",nu)
--- nCov/main.py
+++ nCov/main.py
@@ -32,7 +32,7 @@ plt.rcParams['font.sans-serif'] = ['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
 
 #设置窗口大小
-plt.figure(figsize=(12,7))
+plt.figure(figsize=(12,8))
 
 #基础数据
 days = npy.array(fileReader.dateList)
@@ -45,20 +45,31 @@ for i in range(1,fileReader.numberList.__len__()) :
     diffList.append(fileReader.numberList[i] - fileReader.numberList[i-1])
 diffArray = npy.array(diffList)
 
-#绘制折线图并设置点标，设置标题、X坐标、y坐标
+#绘制全国折线图并设置点标，设置标题、X坐标、y坐标
+plt.subplot(211)
 plt.plot(days,num,'r.-',label="确诊人数", marker='o', markerfacecolor='red', markersize=4)
 plt.plot(days[1:],diffArray,'y.-',label="增加病例", marker='o', markerfacecolor='yellow', markersize=4)
 plt.title("2019-nCoV病毒情况", fontsize=16)
 plt.xticks(days,daysname, rotation=30, fontsize='8')
-plt.xlabel("日期(未标注时间为24点)", fontsize=14)
 plt.ylabel("人数", fontsize=14)
-
-#设置点y坐标
+plt.legend()
+##设置点y坐标
 for a, b in zip(days, num):
-    plt.text(a, b, b, ha='center', va='bottom', fontsize=10)
+    plt.text(a, b, b, ha='center', va='bottom', fontsize=8)
 for c, d in zip(days[1:], diffArray):
-    plt.text(c, d, d, ha='center', va='bottom', fontsize=10)
+    plt.text(c, d, d, ha='center', va='bottom', fontsize=8)
 
+#绘制各个省图片
+plt.subplot(212)
+plt.bar(grabber.provNames, grabber.provNumber)
+
+##设置标签
+plt.xticks(fontsize = 8, rotation=30)
+plt.ylabel("人数", fontsize=14)
+plt.xlabel("图一：日期(未标注为24时)与确诊/新增人数 图二：各省确诊人数", fontsize=12)
+##标上数据
+for e, f in zip(grabber.provNames, grabber.provNumber):
+    plt.text(e, f, f, ha='center', va='bottom', fontsize=8)
 
 #保存文件
 with open("./Date_info.txt","w") as saveDate :
@@ -75,6 +86,4 @@ with open("./Numb_info.txt","w") as saveNumber :
         saveNumber.write(str(number)+" ")
 
 #画！
-plt.legend()
 plt.show()
-
